e2b7d2b29535bc9146c9bc85f01a00c28bd05229
rica441 <pokari.san12@gmail.com>
2020-07-11 01:11:48 +0900

    perf: 冗長なコード削除 #12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,11 +19,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find_by(id: params[:id])
   end
 
   def update
-    @post = Post.find_by(id: params[:id])
     if @post.update(post_params)
       redirect_to posts_url
     else
